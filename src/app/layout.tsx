852bff7e8402d0a0a8fed1b6620f9b9e52a43d9a
clevessonmendonca <wanderbloks311@gmail.com>
2024-01-22 20:11:41 -0300

    docs: speed insights vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { Header } from '@/components/header'
 import { Footer } from '@/components/footer'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           <Header />
           <div className="flex-1">{children}</div>
           <Footer />
+          <SpeedInsights />
         </div>
       </body>
     </html>
