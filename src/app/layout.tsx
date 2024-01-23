db86d81ac3605221678a82122a6bdd8a1d2c7edb
clevessonmendonca <wanderbloks311@gmail.com>
2024-01-23 14:36:31 -0300

    test: google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,42 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { Header } from '@/components/header'
-import { Footer } from '@/components/footer'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-import Head from 'next/head'
-import { GoogleTagManager } from '@next/third-parties/google'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Head from "next/head";
+import { GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Work for Immigration',
+  title: "Work for Immigration",
   description:
-    'Professional and reliable residential cleaning service. Transform your home with Work for Immigration.',
-}
+    "Professional and reliable residential cleaning service. Transform your home with Work for Immigration.",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <GoogleTagManager gtmId="AW-11139734703" />
-      </Head>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-11139734703"
+      ></Script>
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-11139734703');
+            `,
+        }}
+      />
       <body className={inter.className}>
         <div className="flex h-full flex-col">
           <Header />
@@ -34,5 +46,5 @@ export default function RootLayout({
         </div>
       </body>
     </html>
-  )
+  );
 }
