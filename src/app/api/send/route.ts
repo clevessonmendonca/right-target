5e8fc46d5531f886a1d1f75a392a3bb37d75be25
clevessonmendonca <wanderbloks311@gmail.com>
2024-01-28 15:38:54 -0300

    fix: errors

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -4,17 +4,22 @@ import { Resend } from 'resend'
 import { FormSchema } from '@/app/(home)/components/cleaning-form'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
-export async function POST(data: FormSchema) {
+
+export async function POST(form: FormSchema) {
   try {
-    const sendEmail = await resend.emails.send({
+    const { data, error } = await resend.emails.send({
       from: 'Right Target <righttarget.contact@gmail.com>',
       to: ['wanderbloks311@gmail.com'],
       subject: 'Right Target',
-      react: EmailTemplate(data) as React.ReactElement,
+      react: EmailTemplate(form) as React.ReactElement,
     })
 
-    return Response.json(sendEmail)
+    if (error) {
+      return Response.json({ error })
+    }
+
+    return Response.json({ data })
   } catch (error) {
-    console.log(error)
+    return Response.json({ error })
   }
 }
