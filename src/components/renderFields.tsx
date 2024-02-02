e57ba3930efb9ad15a13412afca379ff363a5c7f
clevessonmendonca <wanderbloks311@gmail.com>
2024-02-02 07:49:04 -0300

    remove: component inutil

--- src/components/renderFields.tsx
+++
@@ -1,9 +0,0 @@
-import { Input } from "./ui/input";
-
-export const renderFields = (fieldNames: string[]) => {
-  return fieldNames.map((fieldName) => (
-    <div key={fieldName}>
-        <h1>hello</h1>
-    </div>
-  ));
-};
