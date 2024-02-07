8c54f65ae56c8dbdc194ade711644bbcea9fb3aa
clevessonmendonca <wanderbloks311@gmail.com>
2024-02-07 15:43:37 -0300

    feat: google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,8 @@ import { Toaster } from '@/components/ui/sonner'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const GTM_ID = 'GTM-KXCK2GK5'
+
 export const metadata: Metadata = {
   title: 'Right Target - Cleaning Services',
   description:
@@ -72,6 +74,33 @@ export default function RootLayout({
         />
         <link rel="canonical" href="https://righttargetservice.com/" />
       </head>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', 'your id');
+fbq('track', 'PageView');
+`,
+        }}
+      />
       <Script
         async
         src="https://www.googletagmanager.com/gtag/js?id=AW-11139734703"
@@ -101,6 +130,12 @@ export default function RootLayout({
         }}
       />
       <body className={inter.className}>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
+
         <div className="flex h-full flex-col">
           <Header />
           <div className="flex-1">{children}</div>
